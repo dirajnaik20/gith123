ca317d6064913c32527ca02776c53522695a0c65
diraj <diraj@192.168.1.41>
2022-04-25 16:16:58 +0530

    commit123

---
+++ github123/src/github123/Orange.java
@@ -0,0 +1,16 @@
+package github123;
+
+public class Orange {
+String color;
+Orange(String color)
+{
+this.color=color;
+}
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+Orange greenApple = new Orange(&quot;green&quot;);
+String color = greenApple.getColor();
+System.out.println(&quot;color of apple is &quot; + color);
+
+}
+}
